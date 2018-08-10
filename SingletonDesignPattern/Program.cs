8ebb89a00a4807028bda797b305ee44c0b7c1e38
ibavli <ibrahim.bavli35@gmail.com>
2018-08-10 17:29:04 +0300

    Singleton Design Pattern

--- SingletonDesignPattern/Program.cs
+++ SingletonDesignPattern/Program.cs
@@ -10,6 +10,40 @@ namespace SingletonDesignPattern
     {
         static void Main(string[] args)
         {
+            var maindekisinifimiz = Sinif.SingletonIleSinifUret();
+            maindekisinifimiz.EkranaMerhabaYaz();
+            var maindekisinifimiz2 = Sinif.SingletonIleSinifUret();
+            maindekisinifimiz2.EkranaMerhabaYaz();
+
+            Console.ReadKey();
         }
+    }
+
+    class Sinif
+    {
+        private static Sinif sinif;
+        static object kilit = new object();
+
+        private Sinif()
+        {
+
+        }
+
+        public static Sinif SingletonIleSinifUret()
+        {
+            lock (kilit)
+            {
+                if (sinif == null)
+                {
+                    sinif = new Sinif();
+                }
+            }
+            return sinif;
+        }
+        public void EkranaMerhabaYaz()
+        {
+            Console.WriteLine("Merhaba Dünya");
+        }
+
     }
 }
